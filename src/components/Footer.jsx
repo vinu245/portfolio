57befde513b70c3c33a2cde957a20201b2de9702
Vinutha S <vinuthadevadiga18@gmail.com>
2026-03-28 20:49:20 +0530

    Create Footer component with copyright information and navigation links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <p>&copy; 2026 Your Company. All rights reserved.</p>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href='/about'>About</a></li>
+                    <li><a href='/services'>Services</a></li>
+                    <li><a href='/contact'>Contact</a></li>
+                </ul>
+            </nav>
+        </footer>
+    );
+};
+
+export default Footer;
