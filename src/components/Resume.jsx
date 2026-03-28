2d30063a675d466fd1dafaa4b7ab2d0381a7abdb
Vinutha S <vinuthadevadiga18@gmail.com>
2026-03-28 20:48:59 +0530

    Create Resume component

---
+++ src/components/Resume.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Resume = () => {
+    return (
+        <div>
+            <h2>Download My Resume</h2>
+            <a href="/path-to-your-resume.pdf" download>Click here to download my resume</a>
+        </div>
+    );
+};
+
+export default Resume;
